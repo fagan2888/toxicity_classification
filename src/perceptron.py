8dc350f76fb50e7a8963ec7c094e7a37677d649e
Willian <willianmartinho@gmail.com>
2019-06-04 11:03:17 -0300

    add read from file

--- src/perceptron.py
+++ src/perceptron.py
@@ -1,23 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun May 12 12:10:41 2019
+Created on Fri May 31 19:43:28 2019
 
 @author: willian
 """
 
-import math 
+import numpy as np
+import pandas as pd
 
+from tfidf import *
+
+
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+        
 def sigmoid(x):
-    return (1 / (1 + math.exp(-x))) 
+    return (1 / (1 + np.exp(-x))) 
 
+def degrau(x):
+    if x < 0.5:
+        return 0
+    else:
+        return 1
 
 # Make a prediction with weights
 def predict(row, weights):
-	activation = weights[0]
-	for i in range(len(row)-1):
-		activation += weights[i + 1] * row[i]
-	return sigmoid(activation)
+    activation = weights[0]
+    for i in range(len(row)):
+        activation += weights[i + 1] * row[i]
+    
+    return sigmoid(activation)        
+    #return degrau(activation)
+
 
 # Estimate Perceptron weights using stochastic gradient descent
 def train_weights(train, l_rate, n_epoch):
@@ -34,38 +56,34 @@ def train_weights(train, l_rate, n_epoch):
 		print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
 	return weights
 
-def train_weights_cont(weights, train, l_rate, n_epoch):
-	#weights = [0.0 for i in range(len(train[0]))]
-	for epoch in range(n_epoch):
-		sum_error = 0.0
-		for row in train:
-			prediction = predict(row, weights)
-			error = row[-1] - prediction
-			sum_error += error**2
-			weights[0] = weights[0] + l_rate * error
-			for i in range(len(row)-1):
-				weights[i + 1] = weights[i + 1] + l_rate * error * row[i]
-		print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
-	return weights
 
-# Calculate weights
-dataset = [[2.7810836,2.550537003,0],
-	[1.465489372,2.362125076,0],
-	[3.396561688,4.400293529,0],
-	[1.38807019,1.850220317,0],
-	[3.06407232,3.005305973,0],
-	[7.627531214,2.759262235,1],
-	[5.332441248,2.088626775,1],
-	[6.922596716,1.77106367,1],
-	[8.675418651,-0.242068655,1],
-	[7.673756466,3.508563011,1]]
-l_rate = 0.1
-n_epoch = 5
-weights = train_weights(dataset, l_rate, 10)
-print(weights)
+l_rate = 0.2
+n_epoch = 10000
 
-weights = [0.0 for i in range(len(dataset[0]))]
+# Estimate Perceptron weights using stochastic gradient descent
+#weights = [0.0 for i in range(len(wordDict)+1)]
 
-for train in dataset:
-    weights = train_weights_cont(weights, [train], l_rate, n_epoch)
-print(weights)
+for epoch in range(n_epoch):
+    sum_error = 0.0
+    
+    progress = 0
+    
+    for row, target in zip(train, data.target):
+        prediction = predict(row, weights)
+        error = target - prediction
+        
+        sum_error += error**2
+        
+        weights[0] = weights[0] + l_rate * error
+		
+        for i in range(len(row)):
+            
+            weights[i + 1] = weights[i + 1] + l_rate * error * row[i]
+        
+        progress += 1
+            
+        printProgressBar(progress, len(data), prefix = 'Progress:', suffix = 'Complete', length = 50)
+    print()
+    print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+
+
